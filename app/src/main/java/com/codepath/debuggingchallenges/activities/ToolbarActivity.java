3a1be3d2cbdb5e163d2d4aac0f2eae191075e227
Anna Zhang <annayzhang@fb.com>
2020-06-25 09:28:24 -0700

    Fixed toolbar activity

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ToolbarActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ToolbarActivity.java
@@ -1,9 +1,12 @@
 package com.codepath.debuggingchallenges.activities;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.codepath.debuggingchallenges.R;
 
@@ -15,11 +18,16 @@ public class ToolbarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_toolbar);
 
         // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool);
 
         // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setActionBar(toolbar);
+//         Make sure the toolbar exists in the activity and is not null
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        getSupportActionBar().setTitle("Toolbar Activity");
+
 
         TextView tvDescription = (TextView) findViewById(R.id.tvDescription);
         tvDescription.setText(R.string.hello);
